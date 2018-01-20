d78a59f6d602358bbd6628f6694b7588b2bf9636
Jordan Joseph Lewis <dev_jordanlewis@icloud.com>
2018-01-20 16:30:25 +1100

    make stackview for top label image page accessible

--- JLPagedDemo/Classes/Prebuilt Pages/Top Title w Image/JLPageTopTitleAndImage.swift
+++ JLPagedDemo/Classes/Prebuilt Pages/Top Title w Image/JLPageTopTitleAndImage.swift
@@ -11,6 +11,7 @@ open class JLPageTopTitleAndImage: JLPrebuiltPage {
 
     @IBOutlet public var titleLabel: UILabel!
     @IBOutlet public var imageview: UIImageView!
+    @IBOutlet public var stackview: UIStackView!
     
     open override class func instanceFromNib() -> JLPageTopTitleAndImage {
         return jlpdBundle.loadNibNamed("JLPageTopTitleAndImage", owner: nil, options: nil)!.first as! JLPageTopTitleAndImage
